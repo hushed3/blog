8326e87a5bd349441f4f6b4aedb7153aa09b243f
jhhhh <jhhhh>
2022-10-03 12:29:13 +0800

    更新文章,修改主题切换逻辑

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,36 +15,31 @@ const madeWithLinks = [
 ]
 
 export const Footer = () => {
-  const year = new Date().getFullYear()
+  const year = new Date().getFullYear() === 2022 ? '' : `- ${new Date().getFullYear()}`
   return (
     <footer className="footer">
       <section>
         <nav>
-          <span className="desktop-only">© 2022 - {year} By Hush</span>
+          <span className="desktop-only">© 2022 {year} By Hush</span>
           {links.map((link) => (
-            <a
-              href={link.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              key={link.url}
-            >
+            <a href={link.url} target="_blank" rel="noopener noreferrer" key={link.url}>
               {link.label}
             </a>
           ))}
         </nav>
         <nav>
           {madeWithLinks.map((link) => (
-            <a
-              href={link.url}
-              title={link.label}
-              target="_blank"
-              rel="noopener noreferrer"
-              key={link.url}
-            >
+            <a href={link.url} title={link.label} target="_blank" rel="noopener noreferrer" key={link.url}>
               <span>{link.label}</span>
               <img src={link.icon} alt={link.label} />
             </a>
           ))}
+        </nav>
+
+        <nav>
+          <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">
+            蜀ICP备2022009836号
+          </a>
         </nav>
       </section>
     </footer>
